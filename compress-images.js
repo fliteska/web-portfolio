b623bd7e138ea3a40d588dc4d2bee0027f76e007
Fliteska <fliteska@gmail.com>
2022-03-23 11:56:09 +0000

    Optimise images

---
+++ compress-images.js
@@ -0,0 +1,13 @@
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+
+const files = await imagemin(['assets/img/*.{jpg,png}'], {
+	destination: 'img',
+	plugins: [
+		imageminJpegtran(),
+		imageminPngquant({
+			quality: [0.6, 0.8]
+		})
+	]
+});
